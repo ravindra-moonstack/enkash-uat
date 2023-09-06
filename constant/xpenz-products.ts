4039f954f6131189363384d946f9866ad26af5b5
Kartik Kalia <kartik.kalia@enkash.com>
2023-09-06 16:43:46 +0530

    Xpenz Done

--- constant/xpenz-products.ts
+++ constant/xpenz-products.ts
@@ -1,8 +1,20 @@
+import { heirarchyControl } from "@/app/xpenz";
 import {
   vendorPayment,
   vendorPaymentFilled,
   bulkPayout,
   taxPayment,
+  budgetAndAndvances,
+  budgetAndAndvancesFilled,
+  hierarchyAndControlsFilled,
+  hierarchyAndControls,
+  reimbursements,
+  reimbursementsFilled,
+  policyAndApproval,
+  policyAndApprovalFilled,
+  taxPaymentFilled,
+  scanAndDrop,
+  scanAndDropFilled,
 } from "@/components/header";
 
 const xpenzProducts = [
@@ -12,32 +24,32 @@ const xpenzProducts = [
       {
         name: "Budgets and Advances",
         description: "Track and manage your budget with ease",
-        imageSrc: vendorPayment,
-        imageSrcHovered: vendorPaymentFilled,
+        imageSrc: budgetAndAndvances,
+        imageSrcHovered: budgetAndAndvancesFilled,
       },
       {
         name: "Hierarchy and Controls",
         description: "DIY Hierarchy for approval, policies, and expenses",
-        imageSrc: vendorPayment,
-        imageSrcHovered: vendorPaymentFilled,
+        imageSrc: hierarchyAndControls,
+        imageSrcHovered: hierarchyAndControlsFilled,
       },
       {
         name: "Reimbursement",
         description: "Get reimbursed for your expenses quickly and easily",
-        imageSrc: bulkPayout,
-        imageSrcHovered: vendorPaymentFilled,
+        imageSrc: reimbursements,
+        imageSrcHovered: reimbursementsFilled,
       },
       {
         name: "Policy and Approval Flows",
         description: "Create policies and approval flows on the go",
-        imageSrc: vendorPayment,
-        imageSrcHovered: vendorPaymentFilled,
+        imageSrc: policyAndApproval,
+        imageSrcHovered: policyAndApprovalFilled,
       },
       {
         name: "Scan and Drop Receipts",
         description: "Automate receipt management with OCR<",
-        imageSrc: taxPayment,
-        imageSrcHovered: vendorPaymentFilled,
+        imageSrc: scanAndDrop,
+        imageSrcHovered: scanAndDropFilled,
       },
     ],
     footerText: "Stream Line with Xpenze",
