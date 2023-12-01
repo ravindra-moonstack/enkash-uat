83107b6ed03c9919b9a8f4687cada7bbefe1e47f
Prakash Shukla <prakash.shukla@enkash.com>
2023-12-01 14:08:20 +0530

    virtual card page

---
+++ app/virtual-cards/blog-data.ts
@@ -0,0 +1,34 @@
+const blogData = [
+  {
+    image:
+      "https://www.enkash.com/resources/wp-content/uploads/2022/09/blog_30.2b14ff38.jpg",
+    imageAlt: "Automate Your Spends with SaaS Spend Management Software",
+    title: "Automate Your Spends with SaaS Spend Management Software",
+    date: "Dec 21, 2023",
+    description:
+      "The number of SaaS-based start-ups in India has seen an astonishing rise over the past couple of years. Demand for SaaS products is increasing day by day among industries like e-commerce, healthcare...",
+    link: "https://www.enkash.com/resources/blog/saas-subscription-management-with-virtual-cards/",
+  },
+  {
+    image:
+      "https://www.enkash.com/resources/wp-content/uploads/2023/04/Make-secure-online-payments-with-free-virtual-cards.png",
+    imageAlt: "Make Secure Online Payments with Free Virtual Cards",
+    title: "Make Secure Online Payments with Free Virtual Cards",
+    date: "Dec 21, 2023",
+    description:
+      "While the world is busy transforming everything online, it is better that you choose the solution for payments which is absolutely trustworthy. Now, you must be wondering how one can do that- with EnKash...",
+    link: "https://www.enkash.com/resources/blog/how-to-make-secure-online-payments-with-virtual-cards/",
+  },
+  {
+    image:
+      "https://www.enkash.com/resources/wp-content/uploads/2023/02/A-step-by-step-guide-for-virtual-card-business.jpg",
+    imageAlt: "A Step-by-Step Guide for Virtual Card Business",
+    title: "A Step-by-Step Guide for Virtual Card Business",
+    date: "Dec 21, 2023",
+    description:
+      "A fascinating step towards sound business spending are virtual credit cards. They provide businesses with a safer, more personalized, and transparent way to pay for rent, vendors, GST, bills, and ...",
+    link: "https://www.enkash.com/resources/wp-content/uploads/2023/02/A-step-by-step-guide-for-virtual-card-business.jpg",
+  },
+];
+
+export default blogData;
---
+++ app/virtual-cards/carousel-data.tsx
@@ -0,0 +1,32 @@
+import iconOne from "./img/icon-one.svg";
+import iconTwo from "./img/icon-two.svg";
+import iconThree from "./img/icon-three.svg";
+import iconFour from "./img/icon-four.svg";
+
+const carouselData = [
+  {
+    image: iconOne,
+    alt: "Virtual cards enable quicker payments and efficient accounting",
+    title: "Virtual cards enable quicker payments and efficient accounting",
+  },
+  {
+    image: iconTwo,
+    alt: "Helps in disbursements on the go without engaging in lengthy card setup procedures",
+    title:
+      "Helps in disbursements on the go without engaging in lengthy card setup procedures",
+  },
+  {
+    image: iconThree,
+    alt: "Offers an extra layer of protection to your company’s confidential financial data",
+    title:
+      "Offers an extra layer of protection to your company’s confidential financial data",
+  },
+  {
+    image: iconFour,
+    alt: "Works as a centralized solution for businesses that intend to provide access to multiple employees",
+    title:
+      "Works as a centralized solution for businesses that intend to provide access to multiple employees",
+  },
+];
+
+export default carouselData;
