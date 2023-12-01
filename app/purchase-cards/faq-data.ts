6dc9d0bbfeda9beb45073b980c13f360d75bd8e0
Prakash Shukla <prakash.shukla@enkash.com>
2023-12-01 13:00:00 +0530

    purchase card page complete

---
+++ app/purchase-cards/faq-data.ts
@@ -0,0 +1,31 @@
+const faqData = [
+  {
+    question: "What is a purchase card?",
+    answer: [
+      {
+        heading:
+          "Purchase cards can be used to make purchases as long as you have a balance on it, but they can be used only for purchases of goods and services.",
+      },
+    ],
+  },
+  {
+    question: "What are the benefits of using a purchase card?",
+    answer: [
+      {
+        heading:
+          "Businesses can streamline the procurement process using a purchase card, reduce paperwork, and provide greater visibility and control over spending. A P-card also simplifies expense tracking and reduces the risk of fraud.",
+      },
+    ],
+  },
+  {
+    question: "Who can use a purchase card?",
+    answer: [
+      {
+        heading:
+          "A P-card is typically issued to an employee authorized to make purchases on behalf of the company or organization. The employee is responsible for ensuring that the purchases made with the card comply with the company's policies and procedures.",
+      },
+    ],
+  },
+];
+
+export default faqData;
