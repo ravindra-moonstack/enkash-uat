e5527ea1c7cd2b7319556f80fcb70510c96dd619
Prakash Shukla <prakash.shukla@enkash.com>
2023-12-20 00:05:53 +0530

    gtm tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/_base.scss";
 import "../styles/_about_old.scss";
 
 import "bootstrap/dist/css/bootstrap.css";
+import Script from "next/script";
 
 export interface RootProps {
   children: any;
@@ -22,11 +23,25 @@ const RootLayout = ({ children }: RootProps) => {
           href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200;12..96,300;12..96,400;12..96,500;12..96,600;12..96,700;12..96,800&display=swap"
           rel="stylesheet"
         />
+        {process.env.NEXT_PUBLIC_ENV === "production" && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+            />
+            <Script strategy="afterInteractive">
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+            `}
+            </Script>
+          </>
+        )}
       </head>
       <body>
-        <main className={`app main`}>
-          {children}
-        </main>
+        <main className={`app main`}>{children}</main>
       </body>
     </html>
   );
