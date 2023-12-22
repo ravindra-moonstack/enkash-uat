e8b84283afe3dfb2fcb47cda79996e569274cdb8
Prakash Shukla <prakash.shukla@enkash.com>
2023-12-22 19:15:22 +0530

    final commit

--- components/header/data/resources.ts
+++ components/header/data/resources.ts
@@ -3,30 +3,30 @@ export const resources = [
     name: "Blogs",
     description:
       "Read our product-related blogs and learn how they can transform your business",
-    link: "https://www.enkash.com/resources/blog/",
+    link: "/resources",
   },
   {
     name: "Product Videos",
     description:
       "Watch our product videos for an easy, engaging, and quick understanding",
-    link: "https://www.enkash.com/resources/videos/",
+    link: "/resources/videos",
   },
   {
     name: "Press Release",
     description:
       "Know how EnKash is disrupting the industry with technology and innovation",
-    link: "https://www.enkash.com/resources/press-release/",
+    link: "/resources/press-release",
   },
   {
     name: "News & Media",
     description:
       "Stay updated with the latest news and developments from EnKash",
-    link: "https://www.enkash.com/resources/media-coverage/",
+    link: "/resources/media-coverage",
   },
   {
     name: "Customer Stories",
     description: "Know what our customers have to say after using our products",
-    link: "https://www.enkash.com/resources/customer-stories/",
+    link: "/resources/customer-stories",
   },
 ];
 
