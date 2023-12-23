20620f98a6e7ea6528768675da38180fec4bb078
Prakash Shukla <prakash.shukla@enkash.com>
2023-12-23 12:23:41 +0530

    rewrite rule fix

--- components/header/modal/resources-modal.tsx
+++ components/header/modal/resources-modal.tsx
@@ -100,7 +100,7 @@ const ResourcesModal = () => {
             className={`${styles.resource_arrow_icon} mb-2 color-equity-blue`}
           >
             Read more
-            <a href={blogData.link}>
+            <a href={blogData.link} target="_blank">
               <Image
                 className="ms-2"
                 width={40}
