29c3974ed65c08d4c6211834d8b1454b71fc80f1
Prakash Shukla <prakash.shukla@enkash.com>
2023-12-29 15:22:11 +0530

    popup logic updated

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,12 +1,9 @@
-import PopUp from "../pop-up/pop-up";
 import MobileHeader from "./mobile-header/mobile-header";
 import WebHeader from "./web-header";
 
 const Header = ({ utmSource = "" }: any) => {
   return (
     <>
-      <PopUp />
-
       <div className="desktop-only desktop-header">
         <WebHeader utmSource={utmSource} />
       </div>
