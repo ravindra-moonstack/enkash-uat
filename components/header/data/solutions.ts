003957da84c3499f82098b1e52e92c437f2df13e
Prakash Shukla <prakash.shukla@enkash.com>
2024-01-12 16:03:11 +0530

    sorted list solution

--- components/header/data/solutions.ts
+++ components/header/data/solutions.ts
@@ -1,57 +1,56 @@
 export const solutions = [
   {
-    name: "Cashflow Analytics",
+    name: "APIs & Modules",
     description:
-      "Automate your company's AP, AR, and bank reconciliation seamlessly",
-    link: "/resources",
-  },
-  {
-    name: "Customized Reporting",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Eleifend at interdum massa vitae nunc.",
+      "Enhance your system's functionality with our comprehensive set of APIs and modular solutions.",
     link: "/resources",
   },
   {
     name: "Automated Reconciliation",
     description:
-      "Automate your company's AP, AR, and bank reconciliation seamlessly",
+      "Effortlessly streamline your company's Accounts Payable (AP), Accounts Receivable (AR), and bank reconciliation processes.",
+    link: "/resources",
+  },
+  {
+    name: "CardX",
+    description:
+      "Experience secure and seamless card transactions with our advanced CardX technology.",
+    link: "/resources",
+  },
+  {
+    name: "Cashflow Analytics",
+    description:
+      "Gain valuable insights into your company's financial health through our advanced Cashflow Analytics tools.",
+    link: "/resources",
+  },
+  {
+    name: "Customized Reporting",
+    description:
+      "Create tailored reports to meet your specific business needs with our flexible reporting solutions.",
     link: "/resources",
   },
   {
     name: "Integrations",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Eleifend at interdum massa vitae nunc.",
-    link: "/resources",
-  },
-  {
-    name: "Workflow management",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Eleifend at interdum massa vitae nunc.",
-    link: "/resources",
-  },
-
-  {
-    name: "CardX",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Eleifend at interdum massa vitae nunc.",
-    link: "/resources",
-  },
-  {
-    name: "Loan Origination Solution",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Eleifend at interdum massa vitae nunc.",
+      "Effortlessly integrate our solutions into your existing systems for a seamless workflow.",
     link: "/resources",
   },
   {
     name: "Loan Management Solution",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Eleifend at interdum massa vitae nunc.",
+      "Simplify and optimize the management of loans with our comprehensive Loan Management Solution.",
     link: "/resources",
   },
   {
-    name: "APIs & Modules",
+    name: "Loan Origination Solution",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Eleifend at interdum massa vitae nunc.",
+      "Streamline the loan origination process with our advanced Loan Origination Solution.",
+    link: "/resources",
+  },
+  {
+    name: "Workflow Management",
+    description:
+      "Efficiently manage and optimize your workflows with our advanced Workflow Management tools.",
     link: "/resources",
   },
 ];
