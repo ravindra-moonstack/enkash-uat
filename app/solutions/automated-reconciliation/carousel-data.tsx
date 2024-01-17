ab961704cc697a2f6980dec1ed69c55487d9e328
Prakash Shukla <prakash.shukla@enkash.com>
2024-01-17 15:28:33 +0530

    automated reconcillation

---
+++ app/solutions/automated-reconciliation/carousel-data.tsx
@@ -0,0 +1,31 @@
+import iconOne from "./img/icon-one.svg";
+import iconTwo from "./img/icon-two.svg";
+import iconThree from "./img/icon-three.svg";
+import iconFour from "./img/icon-four.svg";
+
+
+const carouselData = [
+  {
+    image: iconOne,
+    alt: "Login to EnKash or register",
+    title:
+      "Achieve 100 % invoice reconciliation by adopting smarter payments and collections",
+  },
+  {
+    image: iconTwo,
+    alt: "Issue meal cards to employees",
+    title: "Speed up your reconciliation process with the quicker matching of the cash inflows and outflows in real-time",
+  },
+  {
+    image: iconThree,
+    alt: "Notify employees via SMS/email about card activation",
+    title: "Get insights into cash flow and ensure that there are minimal disruptions in financial operations",
+  },
+  {
+    image: iconFour,
+    alt: "Employees must complete KYC to activate their meal cards",
+    title: "Effortlessly improve your ability to meet audit and compliance queries quickly",
+  },
+];
+
+export default carouselData;
