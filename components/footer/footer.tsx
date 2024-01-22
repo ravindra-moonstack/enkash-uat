f06d46a34fdc1b0322fc5f2ea40c8d33a8b86eab
Prakash Shukla <prakash.shukla@enkash.com>
2024-01-22 22:52:00 +0530

    footer changes

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -93,6 +93,11 @@ const Footer = ({ utmSource = "" }: any) => {
                   Vendor Management
                 </Link>
               </li>
+              <li>
+                <Link href="/olympus/payables/seamless-banking/">
+                  Seamless Banking
+                </Link>
+              </li>
             </ul>
           </div>
           <div className="">
@@ -116,6 +121,11 @@ const Footer = ({ utmSource = "" }: any) => {
                 </Link>
               </li>
               <li>
+                <Link href="/olympus/receivables/collection-analytics">
+                  Collection Analytics
+                </Link>
+              </li>
+              <li>
                 <Link href="/olympus/receivables/bulk-collect/">
                   Bulk Collect
                 </Link>
@@ -123,6 +133,9 @@ const Footer = ({ utmSource = "" }: any) => {
               <li>
                 <Link href="/olympus/receivables/invoices/">Invoices</Link>
               </li>
+              <li>
+                <Link href="/olympus/receivables/e-nach">E Nach</Link>
+              </li>{" "}
             </ul>
           </div>
         </div>
