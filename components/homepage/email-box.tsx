8475976170cf9d8c91d3176cf40b6ffb01db98d3
Prakash Shukla <prakash.shukla@enkash.com>
2024-01-31 17:39:39 +0530

    save progress

--- components/homepage/email-box.tsx
+++ components/homepage/email-box.tsx
@@ -6,10 +6,27 @@ import PrimaryButton from "../buttons/primary-button/primary-button";
 
 const EmailBox = () => {
   const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const validateEmail = (inputEmail: any) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(inputEmail);
+  };
+
+  const checkIfEmailValid = (): boolean => {
+    const isValid = validateEmail(email);
+    if (isValid) {
+      setIsValidEmail(true);
+    } else {
+      setIsValidEmail(false);
+    }
+    return isValid;
+  };
 
   return (
     <div className="col-12 d-flex justify-content-center align-items-center">
-      <div className={styles.email_box}>
+      <div className={`${styles.email_box} ${isError ? styles.error : ""}`}>
         <div className="w-50 d-flex align-items-center">
           <input
             type="text"
@@ -17,16 +34,22 @@ const EmailBox = () => {
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
+              checkIfEmailValid();
             }}
           />
         </div>
 
-        <div>
-          <PrimaryButton
-            title="Get Started Now"
-            theme="blue"
-            url={"/sales?email=" + email}
-          />
+        <div onClick={() => setIsError(!isValidEmail)}>
+          {isValidEmail && (
+            <PrimaryButton
+              title="Get Started Now"
+              theme="blue"
+              url={"/sales?email=" + email}
+            />
+          )}
+          {!isValidEmail && (
+            <PrimaryButton title="Get Started Now" theme="blue" />
+          )}
         </div>
       </div>
     </div>
