dc2ab8552a19aee2294297f9de550f7939ace23f
Prakash Shukla <prakash.shukla@enkash.com>
2024-02-17 22:46:41 +0530

    cutomized payment page

---
+++ app/olympus/receivables/customized-payment/faq-data.ts
@@ -0,0 +1,49 @@
+const faqData = [
+  {
+    question: "How do payment pages work?",
+    answer: [
+      {
+        heading:
+          "Businesses can select a template or customize their payment page using their brand guidelines and integrate it seamlessly with their website. Within a few clicks, payment pages can go online where transactions can be done to collect customer information to be processed by the payments gateway.",
+      },
+    ],
+  },
+  {
+    question: "How to integrate payment pages?",
+    answer: [
+      {
+        heading:
+          "Payment pages do not require any technical integration.  Businesses can select or create a template of their choice and start accepting payments in no time.",
+      },
+    ],
+  },
+  {
+    question: "Can I customize the payment page design?",
+    answer: [
+      {
+        heading:
+          "Yes, payment pages can be customized as per your brand requirements.",
+      },
+    ],
+  },
+  {
+    question: "Are payment pages secure?",
+    answer: [
+      {
+        heading:
+          "EnKash payment pages are secure as per industry standard 100% PCI DSS compliant & certified solution.",
+      },
+    ],
+  },
+  {
+    question: "How can I send automated payment receipts?",
+    answer: [
+      {
+        heading:
+          "As you start using the payment page, automated payment receipts will be set by default and sent to customers.",
+      },
+    ],
+  },
+];
+
+export default faqData;
---
+++ app/olympus/receivables/customized-payment/index.ts
@@ -0,0 +1,7 @@
+export { default as laptopScreen } from "./img/laptop-screen.png";
+export { default as blueBg } from "./img/laptop-screen.png";
+export { default as whiteArrow } from "./img/white-arrow.png";
+export { default as faqBg } from "./img/faq-bg.png";
+export { default as caraouselBg } from "./img/carousel-bg.png";
+
+export { default as bannerLottie } from "./img/lottie/banner.json";
