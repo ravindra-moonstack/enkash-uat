e3ee8764699f6ca4eabd15c1f52c1edb537b141c
Kartik Kalia <kartik.kalia@enkash.com>
2024-03-06 23:10:52 +0530

    auto collect

---
+++ app/olympus/receivables/auto-collect/carousel-data.tsx
@@ -0,0 +1,26 @@
+import iconOne from "./img/icon-one.svg";
+import iconTwo from "./img/icon-two.svg";
+import iconThree from "./img/icon-three.svg";
+
+const carouselData = [
+  {
+    image: iconOne,
+    alt: "Generate unique virtual payment addresses for each customer, branch, or department, effortlessly.",
+    title:
+      "Generate unique virtual payment addresses for each customer, branch, or department, effortlessly.",
+  },
+  {
+    image: iconTwo,
+    alt: "Receive payments via NEFT, RTGS, IMPS, or other channels directly to your virtual bank accounts.",
+    title:
+      "Receive payments via NEFT, RTGS, IMPS, or other channels directly to your virtual bank accounts.",
+  },
+  {
+    image: iconThree,
+    alt: "Stay informed with real-time alerts and centralized dashboards, enabling seamless reconciliation and transparency",
+    title:
+      "Stay informed with real-time alerts and centralized dashboards, enabling seamless reconciliation and transparency",
+  },
+];
+
+export default carouselData;
