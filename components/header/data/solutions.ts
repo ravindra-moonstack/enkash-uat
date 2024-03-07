e6a168f12f869ed74be66b856b1259a450b62fa5
Kartik Kalia <kartik.kalia@enkash.com>
2024-03-07 15:24:38 +0530

    icons added + scroll in reveivables

--- components/header/data/solutions.ts
+++ components/header/data/solutions.ts
@@ -29,6 +29,12 @@ export const solutions = [
     description: "Integrate our robust APIs and empower your business",
     link: "/solutions//apis-modules",
   },
+
+  {
+    name: "Integrations",
+    description: "Enhance business processes with integrations",
+    link: "/solutions//integrations",
+  },
 ];
 
 export default solutions;
