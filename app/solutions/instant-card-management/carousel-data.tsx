75941d3a605983264e4e1dc9862eef67635dcf93
Kartik Kalia <kartik.kalia@enkash.com>
2024-04-18 19:47:35 +0530

    cfo suite - sitemap

---
+++ app/solutions/instant-card-management/carousel-data.tsx
@@ -0,0 +1,26 @@
+import iconOne from "./img/icon-one.svg";
+import iconTwo from "./img/icon-two.svg";
+import iconThree from "./img/icon-three.svg";
+
+const carouselData = [
+  {
+    image: iconOne,
+    alt: "Simply log in to your EnKash account, complete your KYC process, and manage your cards with ease.",
+    title:
+      "Simply log in to your EnKash account, complete your KYC process, and manage your cards with ease.",
+  },
+  {
+    image: iconTwo,
+    alt: "Enjoy real-time security, proactive management, and a hassle-free experience in handling corporate cards",
+    title:
+      "Enjoy real-time security, proactive management, and a hassle-free experience in handling corporate cards",
+  },
+  {
+    image: iconThree,
+    alt: "Whether you have a small team or a large enterprise, the system easily adapts to your requirements",
+    title:
+      "Whether you have a small team or a large enterprise, the system easily adapts to your requirements",
+  },
+];
+
+export default carouselData;
---
+++ app/solutions/instant-card-management/faq-data.ts
@@ -0,0 +1,41 @@
+const faqData = [
+  {
+    question: "How do I activate or deactivate my corporate card?",
+    answer: [
+      {
+        heading:
+          "Activation or deactivation of your Card is a simple one-click process. Log in to your account, navigate to the card management system section, and toggle the card's status with ease.",
+      },
+    ],
+  },
+  {
+    question: "Can I activate or deactivate my card from any location?",
+    answer: [
+      {
+        heading:
+          "Yes, you can activate or deactivate your corporate card from anywhere, providing you with the flexibility and control to manage your card's functionality at your convenience.",
+      },
+    ],
+  },
+  {
+    question:
+      "Is there a limit to the number of times I can toggle my card's functionality?",
+    answer: [
+      {
+        heading:
+          "No, there is no limit. You can activate or deactivate as many times as needed.",
+      },
+    ],
+  },
+  {
+    question: "How quickly does the activation or deactivation take effect?",
+    answer: [
+      {
+        heading:
+          "Activation or deactivation is instantaneous. Your card's status is updated in real time, ensuring prompt control over your transactions.",
+      },
+    ],
+  },
+];
+
+export default faqData;
