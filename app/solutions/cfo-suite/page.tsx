bbf4f319df07710cc6adaa4cf45615724d9377c7
Kartik Kalia <kartik.kalia@enkash.com>
2024-05-23 18:26:07 +0530

    removed metadata

--- app/solutions/cfo-suite/page.tsx
+++ app/solutions/cfo-suite/page.tsx
@@ -4,16 +4,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title:
-    "CFO Suite: Empowers CFOs with Approval Workflows & Admin Controls - EnKash",
-  description:
-    "EnKash - The Freedom platform enables CFOs to automate tasks and make data-backed, controlled, and informed decisions through curated information.",
-  alternates: {
-    canonical: "https://www.enkash.com/solutions/cfo-suite/",
-  },
-};
-
 const CfoSuite = () => {
   const router = useRouter();
 
