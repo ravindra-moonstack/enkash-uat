734529362439b2ea93014e98c98b65312080e946
Kartik Kalia <kartik.kalia@enkash.com>
2024-10-22 10:53:19 +0530

    uat error

--- components/breadcrumb/breadbrumb.tsx
+++ components/breadcrumb/breadbrumb.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
-import styles from "./Breadcrumb.module.scss";
+import styles from "./breadcrumb.module.scss";
 import { backArrowWhiteBg } from ".";
 import Image from "next/image";
 
