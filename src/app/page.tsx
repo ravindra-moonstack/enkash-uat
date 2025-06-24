0a3cc1a2eb99e8dc159b04217fa5f3b4f38f4871
Dheeraj <dheerajgill25@gmail.com>
2025-06-24 11:48:44 +0530

    changes in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Header from "../../components/header/header"
-import Heading from "../../components/heading/heading"
 import PaymentGateway from "./payment-gateway/page"
 
 export default function Home() {
