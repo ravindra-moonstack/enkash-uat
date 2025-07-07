a66d8028f0fad322131d39837db395be1baaeba8
Dheeraj <dheerajgill25@gmail.com>
2025-07-07 11:46:01 +0530

    changes

--- components/featureCard/feature-card.tsx
+++ components/featureCard/feature-card.tsx
@@ -17,36 +17,43 @@ const FeatureCard = ({
   cardImage,
 }: CardProps) => {
   return (
-    <div
-      className={`d-flex flex-column justify-content-between text-start ${styles.card_body}`}
-    >
-      <div className={styles.card_top}>
-        {cardImage && (
-          <div className={styles.image_wrapper}>
-            <Image
-              src={cardImage}
-              alt="card visual"
-              className={styles.card_image}
-              width={65}
-              height={65}
-            />
-          </div>
-        )}
-        <div className={styles.title_wrapper}>
-          {titleHtml ? (
-            <div className={styles.titleHtml}>{titleHtml}</div>
-          ) : (
-            whiteTitle && (
-              <Heading title={whiteTitle} color="black" size="h3" weight="5" />
-            )
+    <div className={`d-flex flex-column text-start ${styles.card_body} me-2`}>
+      <div className="d-flex flex-column flex-grow-1">
+        <div className={styles.card_top}>
+          {cardImage && (
+            <div className={styles.image_wrapper}>
+              <Image
+                src={cardImage}
+                alt="card visual"
+                className={styles.card_image}
+                width={65}
+                height={65}
+              />
+            </div>
           )}
+          <div className={styles.title_wrapper}>
+            {titleHtml ? (
+              <div className={styles.titleHtml}>{titleHtml}</div>
+            ) : (
+              whiteTitle && (
+                <Heading
+                  title={whiteTitle}
+                  color="black"
+                  size="h3"
+                  weight="5"
+                />
+              )
+            )}
+          </div>
         </div>
-      </div>
 
-      <div
-        className={`${styles.description} `}
-        dangerouslySetInnerHTML={{ __html: description }}
-      ></div>
+        <div
+          className={`${styles.description} mt-3`} // add space above description if needed
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></div>
+      </div>
+      {/* Extra space at the bottom */}
+      <div className="mt-4" /> {/* You can increase mt-4 to mt-5 etc. */}
     </div>
   )
 }
