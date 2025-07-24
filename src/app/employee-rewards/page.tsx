1e5f81e8c92c302f1fefbd70e32feb76dac86d2c
Dheeraj <dheerajgill25@gmail.com>
2025-07-24 18:35:40 +0530

    rewards page created

---
+++ src/app/employee-rewards/page.tsx
@@ -0,0 +1,431 @@
+"use client"
+import Image from "next/image"
+import styles from "./page.module.scss"
+import { allProductSections, cardsData, cardType } from "./data"
+import faqData, { SecondfaqData } from "./faq-data"
+import { Header, FAQHtml, Footer } from "@/components"
+import {
+  blueArrow,
+  groupIcon,
+  paymentSummary,
+  whiteArrow,
+  mealCardImage,
+  activationIcon,
+  realTimeIcon,
+  faqBg,
+} from "."
+
+import RectangleButton from "@/components/buttons/rectangle-button/rectangle-button"
+import CustomBreadcrumb from "@/components/breadcrumb/breadbrumb"
+import LogoSlider from "@/components/logo-slider/logo-slider"
+import TalkToSales from "@/components/mobile-talks-to-sales/mobile-talk-to-sales"
+import ScrollableCardsSection from "@/components/scrollable-cards-section/scrollableCardsSection"
+import DynamicHeading from "@/components/dynamicHeading/dynamic-heading"
+import RewardsCarousel from "@/components/rewards-carousel/rewards-carousel"
+import SecondFaqHtml from "@/components/second-faq/secondFaqHtml"
+
+const mergedCards = cardType.flatMap((section) => section.items)
+
+const EmployeeRewards = (): React.JSX.Element => {
+  return (
+    <div className={`color-white ${styles.home_container}`}>
+      <Header utmSource="expense_management" />
+
+      <TalkToSales />
+
+      <div className={`${styles.first_row} `}>
+        <div className="max-w-auto">
+          <div className="d-flex flex-column flex-md-row">
+            <div className="col-12 col-md-6 d-flex flex-column">
+              <div className="d-flex">
+                <CustomBreadcrumb
+                  items={[
+                    { name: "Home", url: "/" },
+                    {
+                      name: "Rewards",
+                      url: "/products/rewards",
+                    },
+                    {
+                      name: "Employee Rewards",
+                      url: "/employee-rewards",
+                    },
+                  ]}
+                />
+              </div>
+              <div
+                className={`${styles.first_row_title} d-md-flex text-center  flex-column flex-md-row `}
+              >
+                <DynamicHeading
+                  content={[
+                    {
+                      title: "Rent Payments",
+                      color: "color-equity-blue underline",
+                    },
+                  ]}
+                  headingTag="h4"
+                  className="f-4"
+                />
+              </div>
+              <div
+                className={`text-center text-md-start ${styles.first_row_content}  `}
+              >
+                <div className="d-inline   pt-3 pt-md-0">
+                  <DynamicHeading
+                    content={[
+                      {
+                        title:
+                          "Motivate Your Valuable Workflow with Employee Rewards",
+                        color: "color-black",
+                      },
+                    ]}
+                    headingTag="h1"
+                    className="f-7"
+                  />
+                </div>
+
+                <div className="d-flex mt-3 mb-3 text-center text-md-start ">
+                  <DynamicHeading
+                    content={[
+                      {
+                        title:
+                          "Recognize achievements, celebrate milestones, and boost morale with EnKash’s all-in-one employee rewards and recognition platform.",
+                        color: "color-black",
+                      },
+                    ]}
+                    headingTag="h5"
+                    className="f-4"
+                  />
+                </div>
+
+                <div className="d-flex flex-column align-items-center align-items-md-start">
+                  <Image
+                    src={groupIcon}
+                    alt="card visual"
+                    className={styles.group_logo}
+                  />
+                  <div
+                    className={`${styles.first_row_button} d-flex flex-row  align-items-center`}
+                  >
+                    <div>
+                      <RectangleButton
+                        title="Get Started  "
+                        theme="blue"
+                        url="/sales/?source=expense_management"
+                      />
+                    </div>
+                    <div>
+                      <RectangleButton
+                        title="API Doc"
+                        theme="outline-blue"
+                        url="/sales/?source=expense_management"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 d-flex justify-content-center d-md-flex">
+              <div
+                className={`${styles.right_img} position-relative w-100 h-100 d-flex`}
+              >
+                <Image
+                  src={paymentSummary}
+                  alt="card visual"
+                  style={{
+                    objectFit: "contain",
+                    maxHeight: "672px",
+                  }}
+                  className="w-100"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <LogoSlider />
+      </div>
+
+      <div className={`row ${styles.second_row} `}>
+        <div className="d-flex justify-content-center  flex-column gap-32  align-items-center max-w-auto">
+          <div className="d-inline justify-content-center  align-items-center text-center">
+            <DynamicHeading
+              content={[
+                {
+                  title: "Recognize & Retain Employees",
+                  color: "color-white",
+                },
+              ]}
+              headingTag="h3"
+              className="f-3"
+            />
+          </div>
+
+          <div className={` d-flex ${styles.section}`}>
+            {" "}
+            <div
+              className={` d-flex flex-column justify-content-center align-items-center ${styles.card}`}
+            >
+              <div className={` d-flex ${styles.outerCard}`}>
+                <DynamicHeading
+                  content={[
+                    {
+                      title: "100%",
+                      color: "color-white",
+                    },
+                  ]}
+                  headingTag="h2"
+                  className="f-7"
+                />
+              </div>
+              <div
+                className={`py-2 d-flex flex-column justify-content-center align-items-center ${styles.innerCard}`}
+              >
+                {" "}
+                <DynamicHeading
+                  content={[
+                    {
+                      title: "Extensive Reward Options",
+                      color: "color-white",
+                    },
+                  ]}
+                  headingTag="h5"
+                  className="f-4"
+                />
+              </div>
+            </div>
+            <div
+              className={` d-flex flex-column justify-content-center align-items-center ${styles.card}`}
+            >
+              <div className={` d-flex ${styles.outerCard}`}>
+                <Image src={activationIcon} alt="icon" />
+              </div>
+              <div
+                className={`py-2 d-flex flex-column justify-content-center align-items-center ${styles.innerCard}`}
+              >
+                <DynamicHeading
+                  content={[
+                    {
+                      title: "Instant Reward Redemption",
+                      color: "color-white",
+                    },
+                  ]}
+                  headingTag="h5"
+                  className="f-4"
+                />
+              </div>
+            </div>
+            <div
+              className={` d-flex flex-column justify-content-center align-items-center ${styles.card}`}
+            >
+              <div className={` d-flex ${styles.outerCard}`}>
+                <Image src={realTimeIcon} alt="icon" />
+              </div>
+              <div
+                className={`py-2 d-flex flex-column justify-content-center align-items-center ${styles.innerCard}`}
+              >
+                <DynamicHeading
+                  content={[
+                    {
+                      title: "Easy Reward Disbursement",
+                      color: "color-white",
+                    },
+                  ]}
+                  headingTag="h5"
+                  className="f-4"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.third_row}>
+        <div className={`relative max-w-auto`}>
+          <div className={`${styles.title} text-center pb-md-5 pb-3`}>
+            <DynamicHeading
+              content={[
+                {
+                  title: "How to Distribute ",
+                  color: "color-black",
+                },
+                {
+                  title: "Employee Rewards ",
+                  color: "color-equity-blue",
+                },
+              ]}
+              headingTag="h2"
+              className="f-6"
+            />
+          </div>
+          <div className="row">
+            <div className="col-md-6 col-12">
+              <ScrollableCardsSection
+                cardsData={cardsData}
+                showScroll={true}
+                maxHeight="400px"
+                buttonTitle="Get Started"
+                buttonTheme="outline-blue"
+                buttonUrl="/sales/?source=receivables"
+              />
+            </div>
+            <div className="col-md-6 col-12">
+              <div>
+                <Image
+                  src={mealCardImage}
+                  alt="card background"
+                  className="w-100 h-100"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${styles.slider_row} relative`}>
+        <div className={`${styles.title} text-center pb-5 max-w-auto`}>
+          <div className={` pb-3  d-inline`}>
+            <DynamicHeading
+              content={[
+                {
+                  title: "Other Products ",
+                  color: "color-black",
+                },
+                {
+                  title: "to Build High-Performing Teams",
+                  color: "color-equity-blue",
+                },
+              ]}
+              headingTag="h2"
+              className="f-5"
+            />
+          </div>
+        </div>
+        <RewardsCarousel
+          title="All Features"
+          subtitle="Combine all use cases"
+          data={mergedCards.map((card) => ({
+            title: card.titleHtml,
+            description: card.description,
+            image: card.cardImage,
+            link: card.url,
+          }))}
+        />
+      </div>
+
+      <div className={`${styles.eigth_row} relative`}>
+        <div className={`${styles.faqSection} text-start  max-w-auto`}>
+          <div className={`${styles.title} text-start pb-5`}>
+            <DynamicHeading
+              content={[
+                {
+                  title: "Rewards That Work For  ",
+                  color: "color-black",
+                },
+                {
+                  title: "Employers and Employees! ",
+                  color: "color-equity-blue",
+                },
+              ]}
+              headingTag="h2"
+              className="f-6"
+            />
+          </div>
+          <div className="row align-items-end ">
+            <div className="col-md-6 col-12 pb-4">
+              {" "}
+              <SecondFaqHtml SecondfaqData={SecondfaqData} />
+            </div>
+            <div className="col-md-6 col-12 d-md-block d-none">
+              <div className={styles.faq_bg}>
+                <Image src={faqBg} alt="background image" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${styles.fifth_row} `}>
+        <div className="d-flex justify-content-center  flex-column gap-32   align-items-center max-w-auto">
+          <div className="d-flex justify-content-center  align-items-center text-center">
+            <DynamicHeading
+              content={[
+                {
+                  title: "Ready to turn hard work into heartfelt rewards?",
+                  color: "color-white",
+                },
+              ]}
+              headingTag="h3"
+              className="f-4 bannerHeading"
+            />
+          </div>
+          <div className={`${styles.get_started_button} `}>
+            <RectangleButton
+              title="Get Started  "
+              theme="outline-blue"
+              actionImage={blueArrow}
+              hoverImage={whiteArrow}
+              url="/sales/"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={`${styles.faq_new_row}  relative`}>
+        <div className={`${styles.faqSection} text-start max-w-auto `}>
+          <div className={`${styles.title} text-start  pb-md-5 pb-2`}>
+            <DynamicHeading
+              content={[
+                {
+                  title: "Frequently Asked Questions (",
+                  color: "color-black",
+                },
+                {
+                  title: "FAQs",
+                  color: "color-equity-blue",
+                },
+                {
+                  title: ")",
+                  color: "color-black",
+                },
+              ]}
+              headingTag="h2"
+              className="f-5"
+            />
+          </div>
+          <div className="d-flex flex-column flex-md-row justify-content-between">
+            <div>
+              <div>
+                <DynamicHeading
+                  content={[
+                    {
+                      title: "Have more questions?",
+                      color: "color-dark-grey",
+                    },
+                  ]}
+                  headingTag="h3"
+                  className="f-5"
+                />
+              </div>
+              <div className="mt-3 d-none d-md-block">
+                <RectangleButton
+                  title="Get started today"
+                  theme="border-gray"
+                  actionImage={blueArrow}
+                  hoverImage={whiteArrow}
+                  iconSize={15}
+                  url="/sales/?source=receivables"
+                />
+              </div>
+            </div>
+            <div className={`${styles.faqData}`}>
+              <FAQHtml faqData={faqData} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default EmployeeRewards
---
+++ src/app/offers/data.tsx
@@ -0,0 +1,124 @@
+import crouselIconOne from "./img/crouselIconOne.svg"
+import crouselIconTwo from "./img/crouselIconTwo.svg"
+import crouselIconThree from "./img/crouselIconThree.svg"
+import crouselIconFour from "./img/crouselIconFour.svg"
+import crouselIconFive from "./img/crouselIconFive.svg"
+import crouselIconSix from "./img/crouselIconSix.svg"
+import giftCard from "./img/gift-card.webp"
+import offers from "./img/offers.webp"
+import incentive from "./img/incentive.webp"
+import vouchers from "./img/vouchers.webp"
+
+import {
+  scrollIconOne,
+  scrollIconThree,
+  scrollIconTwo,
+} from "."
+
+const cardsData = [
+  {
+    title: "Complete KYC",
+    description: "Sign up on EnKash and complete your KYC.",
+    icon: scrollIconOne,
+  },
+  {
+    title: "Select Offers",
+    description:
+      "Visit `Partnered Services` on the dashboard. Explore our curated corporate offers and select as per your requirements.",
+    icon: scrollIconTwo,
+  },
+  {
+    title: "Apply For Offer",
+    description:
+      "Read the terms and conditions and click on `Apply Now` to get the offer.",
+    icon: scrollIconThree,
+  },
+]
+
+const cardType = [
+  {
+    title: "Expense Management",
+    subtitle: "Manage every spend effortlessly",
+    items: [
+      {
+        titleHtml: "Gift Cards",
+        description:
+          "Empower teams with versatile, prepaid gift cards for seamless rewards and recognition.",
+        cardImage: giftCard,
+        url: "/gift-cards/",
+      },
+      {
+        titleHtml: "Offers",
+        description:
+          "Unlock exclusive deals and discounts to boost employee engagement and motivation.",
+        cardImage: offers,
+        url: "/offers/",
+      },
+      {
+        titleHtml: "Channel Incentives ",
+        description:
+          "Drive partner performance with strategic rewards, commissions, and loyalty programs.",
+        cardImage: incentive,
+        url: "/channel-incentives/",
+      },
+      {
+        titleHtml: "Brand Vouchers ",
+        description:
+          "Provide employees with top-brand vouchers for flexible and meaningful appreciation.",
+        cardImage: vouchers,
+        url: "/vouchers/",
+      },
+    ],
+  },
+]
+const allProductSections = [
+  {
+    title: "Expense Management",
+    subtitle: "Manage every spend effortlessly",
+    items: [
+      {
+        title: "Recurring Billing Setup",
+        description:
+          "Easily schedule recurring rent payments aligned with your rental agreements.",
+        image: crouselIconOne,
+      },
+      {
+        title: "Payment Notifications ",
+
+        description:
+          "Both tenants and landlords get instant alerts on payment status, ensuring transparency.",
+        image: crouselIconTwo,
+      },
+
+      {
+        title: "Split Payment Options ",
+        description:
+          "Divide rent across multiple payers or payment modes effortlessly.",
+        image: crouselIconThree,
+      },
+      {
+        title: "Customizable Payment Schedules",
+
+        description:
+          "Tailor payment dates to fit your cash flow without hassle.",
+        image: crouselIconFour,
+      },
+      {
+        title: "Dynamic Rent Adjustments",
+
+        description:
+          "Handle rent changes, prorated amounts, or additional fees smoothly.",
+        image: crouselIconFive,
+      },
+      {
+        title: "Detailed Rental Insights",
+
+        description:
+          "Access monthly rent reports and analytics to track trends and expenses.",
+        image: crouselIconSix,
+      },
+    ],
+  },
+]
+
+export { cardsData, allProductSections, cardType }
