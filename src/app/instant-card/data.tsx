bc1e77eca83ed57386a29e128c78a6357ce984fc
Dheeraj <dheerajgill25@gmail.com>
2025-08-01 18:36:55 +0530

    cards new pages

---
+++ src/app/instant-card/data.tsx
@@ -0,0 +1,190 @@
+import iconOne from "./img/subscribtionIcon.svg"
+import iconTwo from "./img/employeeIcon.svg"
+import iconThree from "./img/vendorIcon.svg"
+import iconFour from "./img/travelExpenceIcon.svg"
+import sliderOneIcon from "./img/sliderOneIcon.svg"
+import sliderTwoIcon from "./img/sliderTwoIcon.svg"
+import sliderThreeIcon from "./img/sliderThreeIcon.svg"
+import sliderFourIcon from "./img/sliderFourIcon.svg"
+import firstIcon from "./img/icon-one.svg"
+import secondIcon from "./img/icon-two.svg"
+import thirdIcon from "./img/icon-three.svg"
+import accesableIcon from "./img/accesable-icon.svg"
+import trackableIcon from "./img/trackable-icon.svg"
+import denominationIcon from "./img/denomination-icon.svg"
+
+import {
+  fuelCardImg,
+  mealCardImg,
+  purchesCardImg,
+  sassCardImg,
+  termsAndConCardImg,
+  virtualCardImg,
+} from "."
+
+const intantActionData = [
+  {
+    title: "Sign Up",
+    description: "Sign up on EnKash with all the necessary company details.",
+    icon: firstIcon,
+  },
+
+  {
+    title: "Add Funds ",
+    description:
+      "Add funds to the company account and request gift cards whenever necessary.",
+    icon: secondIcon,
+  },
+  {
+    title: "Distribution",
+    description:
+      "Enterprises can distribute these gift cards with preset amounts based on the occasion.",
+    icon: thirdIcon,
+  },
+]
+
+const cardData = [
+  {
+    icon: accesableIcon,
+    title: "Easily Accessible",
+  },
+  {
+    icon: trackableIcon,
+    title: "Trackable ",
+  },
+  {
+    icon: denominationIcon,
+    title: "Multiple Denominations",
+  },
+]
+
+const spendAnalyticsData = [
+  {
+    title: "Employee Rewards",
+    description:
+      "Appreciate your workforce with employee rewards they deserve.",
+    icon: iconOne,
+  },
+
+  {
+    title: "Offers",
+    description:
+      "Unlock exclusive deals and discounts to boost employee engagement and motivation.",
+    icon: iconTwo,
+  },
+  {
+    title: "Channel Incentives ",
+    description:
+      "Drive partner performance with strategic rewards, commissions, and loyalty programs.",
+    icon: iconThree,
+  },
+  {
+    title: "Brand Vouchers ",
+    description:
+      "Provide employees with top-brand vouchers for flexible and meaningful appreciation.",
+    icon: iconFour,
+  },
+]
+
+const cardType = [
+  {
+    titleHtml: "Fuel Card",
+    description:
+      "Track and control fuel spends with real-time limits, spend visibility, and GST-compliant invoices.",
+    cardImage: fuelCardImg,
+  },
+  {
+    titleHtml: "Digital Marketing Card",
+    description:
+      "Simplify ad payments across platforms while staying within budget and preventing overspending.",
+    cardImage: purchesCardImg,
+  },
+  {
+    titleHtml: "SaaS Card",
+    description:
+      "Manage recurring SaaS subscriptions with auto-renewal tracking and vendor-wise expense contro",
+    cardImage: sassCardImg,
+  },
+  {
+    titleHtml: "Payroll Card",
+    description:
+      "Pay employees, freelancers, or gig workers directly with zero bank dependency or delays.",
+    cardImage: mealCardImg,
+  },
+  {
+    titleHtml: "Meal Card",
+    description:
+      "Offer tax-free meal benefits to employees with wide acceptance and full usage transparency.",
+    cardImage: mealCardImg,
+  },
+  {
+    titleHtml: "T&E Card",
+    description:
+      "Streamline business travel spends—set budgets, track usage, and automate travel expense reconciliation",
+    cardImage: termsAndConCardImg,
+  },
+  {
+    titleHtml: "Purchase Card",
+    description:
+      "Centralize vendor and business purchases with approval workflows, spend controls, and GST capture.",
+    cardImage: virtualCardImg,
+  },
+  {
+    titleHtml: "Fleet Card",
+    description:
+      "Digitize fleet expenses such as fuel, tolls, and repairs, while tracking usage per vehicle or driver",
+    cardImage: termsAndConCardImg,
+  },
+
+  {
+    titleHtml: "Forex Card",
+    description:
+      "Enable safe and compliant international spends with multi-currency loading and real-time transaction control.",
+    cardImage: sassCardImg,
+  },
+]
+
+const allProductSections = [
+  {
+    title: "Expense Management",
+    subtitle: "Manage every spend effortlessly",
+    items: [
+      {
+        title: "Accessibility ",
+        description:
+          "The EnKash way of issuing gift cards is simplified, making it easy to access digital cards from anywhere.",
+        image: sliderOneIcon,
+        link: "/payment-links/",
+      },
+      {
+        title: "Easy To Track",
+        description:
+          "Gift cards on EnKash can be easily tracked and blocked to avoid loss and misuse.",
+        image: sliderTwoIcon,
+        link: "/payment-page/",
+      },
+      {
+        title: "Multiple Denominations ",
+        description:
+          "Gift cards are available in multiple denominations of up to Rs.10,000; one can choose the card value per their requirement. Limits are configurable based on your choice.",
+        image: sliderThreeIcon,
+        link: "/payment-button/",
+      },
+      {
+        title: "Acceptability",
+        description:
+          "Gift cards by EnKash are widely accepted across many places.",
+        image: sliderFourIcon,
+        link: "/qr-codes/",
+      },
+    ],
+  },
+]
+
+export {
+  intantActionData,
+  spendAnalyticsData,
+  cardType,
+  allProductSections,
+  cardData,
+}
