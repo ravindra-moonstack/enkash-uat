78bea9f96ad28d0eae14264271b974de153e02b7
Dheeraj <dheerajgill25@gmail.com>
2025-08-01 18:36:55 +0530

    cards new pages

---
+++ src/app/diy-card/faq-data.tsx
@@ -0,0 +1,66 @@
+import { FAQProps } from "../../../components/faq-new/faq"
+
+// ✅ Create a type that excludes `index` & `answerVisible` & `onToggleAnswerVisibility`
+type FAQDataItem = Omit<
+  FAQProps,
+  "index" | "answerVisible" | "onToggleAnswerVisibility"
+>
+
+const faqData: FAQDataItem[] = [
+  {
+    question: "What is a DIY card module?",
+    answer: [
+      {
+        heading:
+          "The DIY card module is a solution for businesses to manage corporate card expenses. It offers customizable spending limits, real-time expense tracking, and easy card management with a user-friendly dashboard. This centralized solution simplifies processes like issuing purpose-specific cards and disabling cards, ensuring efficient control and optimization of overall business expenditure.",
+      },
+    ],
+  },
+  {
+    question: "How can I apply for the DIY card solution?",
+    answer: [
+      {
+        heading:
+          "To apply for the DIY card solution, get in touch with us and share your requirements.",
+      },
+    ],
+  },
+  {
+    question: "What documents are needed to apply for this solution?",
+    answer: [
+      {
+        heading:
+          "To apply for this solution, follow these steps:\n\nCreate an EnKash account by completing a simple registration process, and verify your account with KYC details.\n\nChoose the type of business entity: Sole Proprietorship, Partnership, Private Limited Company, Public Limited Company, or others.\n\nProvide the following documents:\n\n• Sole Proprietorship: Business financial statements for the past two years, along with the owner's credentials.\n• Partnership: Business financial statements for the past two years and personal financial statements of the partners.\n• Private Limited Company: Business financial statements for the past two years and the finances of the directors.\n• Public Limited Company: Audited financial statements for the past two years, details of the company structure, and the board of directors.\n\nSubmit financials based on your business type:\n• GSTIN certificate.\n• PAN details of the business.\n• Owner/Partner/Director details.\n\nProvide credit scores for the company or proprietor/partner/directors.\n\nInclude additional documents such as:\n• Partnership deed for partnership firms.\n• Shareholding structure for private limited companies.\n• Shareholding pattern for public limited companies.\n\nSubmit any other relevant documents requested during the final credit assessment to ensure the best possible credit limit for your business.",
+      },
+    ],
+  },
+  {
+    question: "How does a DIY card solution help with audits?",
+    answer: [
+      {
+        heading:
+          "We provide a free smart dashboard along with your card that enables you to track and manage expenses in real time. You can also update entries in real time and keep track of approval status. All these factors will help you with audits.",
+      },
+    ],
+  },
+  {
+    question: "How can I prevent fraud with the DIY card solution?",
+    answer: [
+      {
+        heading:
+          "To prevent fraud using the DIY card solution, you can employ OTP authorization for secure card usage. Additionally, if the card is lost or misplaced, promptly block it using the DIY dashboard. This ensures added security and prevents unauthorized access to the card.",
+      },
+    ],
+  },
+  {
+    question: "How can I control expenses with the DIY card solution?",
+    answer: [
+      {
+        heading:
+          "To control expenses with the DIY card solution, utilize the DIY dashboard to monitor and analyze your spending. Receive regular analytics for a comprehensive overview, allowing you to optimize business spend and maintain control over expenses.",
+      },
+    ],
+  },
+]
+
+export default faqData
---
+++ src/app/diy-card/page.tsx
@@ -0,0 +1,514 @@
+"use client"
+import Image from "next/image"
+import { space } from "@/common/constant"
+import styles from "./page.module.scss"
+import {
+  allProductSections,
+  cardType,
+  intantActionData,
+  spendAnalyticsData,
+} from "./data"
+
+import { Header, Heading, Footer, FAQHtml } from "@/components"
+
+import {
+  blueArrow,
+  whiteArrow,
+  mealCardImage,
+  heroCardImg,
+  podiumImage,
+  realTimeExpenseIcon,
+  realTimeExpense,
+  fraudProtectionIcon,
+  fraudProtection,
+  streamlinedReimbursementIcon,
+  streamlinedReimbursement,
+  costControlSavingsIcon,
+  costControlSavings,
+  wideAcceptanceNetworkIcon,
+  wideAcceptanceNetwork,
+  integrationAudit,
+  seemlessIntegration,
+  integrationAuditIcon,
+  seemlessIntegrationIcon,
+  instantActionImg,
+} from "."
+
+import RectangleButton from "@/components/buttons/rectangle-button/rectangle-button"
+import TalkToSales from "@/components/mobile-talks-to-sales/mobile-talk-to-sales"
+import CustomBreadcrumb from "@/components/breadcrumb/breadbrumb"
+import faqData from "./faq-data"
+import LogoSlider from "@/components/logo-slider/logo-slider"
+import CardProduct from "@/components/card-product/card-product"
+import AllProducts from "@/components/all-products/all-products"
+import StepCard from "@/components/stepCard/stepCard"
+import AllInOnePolicy from "@/components/all-in-one-policy/all-in-one-policy"
+import DynamicHeading from "@/components/dynamicHeading/dynamic-heading"
+import CardStacking from "@/components/cardStacking/cardStacking"
+
+// const showScroll = cardsData.length > 3
+const mergedCards = allProductSections.flatMap((section) => section.items)
+const cards = [
+  {
+    color: "#fff",
+    content: (
+      <>
+        <AllInOnePolicy
+          icon={realTimeExpenseIcon}
+          title="Purpose-Made Cards for Every Need"
+          description="Issue cards specifically designed for your unique use cases, such as travel, SaaS, or marketing campaigns. Configure individual card settings to match your organization’s requirements."
+          image={realTimeExpense}
+          buttonUrl="/sales"
+          maxImageHeight="300px"
+        />
+      </>
+    ),
+  },
+  {
+    color: "#eee",
+    content: (
+      <>
+        <AllInOnePolicy
+          icon={fraudProtectionIcon}
+          title="Precise Spending Controls"
+          description="Define spending rules for teams or individuals. Customize limits based on categories, amounts, or timeframes to maintain budget discipline."
+          image={fraudProtection}
+          buttonUrl="/sales"
+          maxImageHeight="300px"
+        />
+      </>
+    ),
+  },
+  {
+    color: "#fff",
+    content: (
+      <>
+        <AllInOnePolicy
+          icon={streamlinedReimbursementIcon}
+          title="Real-Time Monitoring"
+          description="Track transactions as they happen. Gain instant insights into where and how funds are being used, ensuring better financial decisions."
+          image={streamlinedReimbursement}
+          buttonUrl="/sales/?source=expense_management"
+          maxImageHeight="300px"
+        />
+      </>
+    ),
+  },
+  {
+    color: "#eee",
+    content: (
+      <>
+        <AllInOnePolicy
+          icon={costControlSavingsIcon}
+          title="Built-In Approval Workflows"
+          description="Simplify internal processes with predefined approval workflows that align with company policies. Save time and reduce repetitive tasks."
+          image={costControlSavings}
+          buttonUrl="/sales"
+          maxImageHeight="300px"
+        />
+      </>
+    ),
+  },
+  {
+    color: "#eee",
+    content: (
+      <>
+        <AllInOnePolicy
+          icon={wideAcceptanceNetworkIcon}
+          title="Enhanced Vendor Payments"
+          description="Ensure timely payments to vendors, strengthening business relationships and unlocking the potential for better payment terms."
+          image={wideAcceptanceNetwork}
+          buttonUrl="/sales"
+          maxImageHeight="300px"
+        />
+      </>
+    ),
+  },
+  {
+    color: "#eee",
+    content: (
+      <>
+        <AllInOnePolicy
+          icon={integrationAuditIcon}
+          title="Integrated Audit Support"
+          description="Keep financial operations transparent and compliant with automated reconciliation and comprehensive transaction records."
+          image={integrationAudit}
+          buttonUrl="/sales"
+          maxImageHeight="300px"
+        />
+      </>
+    ),
+  },
+  {
+    color: "#eee",
+    content: (
+      <>
+        <AllInOnePolicy
+          icon={seemlessIntegrationIcon}
+          title="Seamless Integration"
+          description="Easily connect with popular accounting tools like Tally and Zoho to simplify data management and reporting."
+          image={seemlessIntegration}
+          buttonUrl="/sales"
+          maxImageHeight="300px"
+        />
+      </>
+    ),
+  },
+]
+const DiyCards = (): React.JSX.Element => {
+  return (
+    <div className={`color-white  ${styles.home_container}`}>
+      <Header utmSource="expense_management" />
+      <TalkToSales />
+      <div className={`${styles.first_row}`}>
+        <div className="max-w-auto ">
+          <div className="d-flex">
+            <CustomBreadcrumb
+              items={[
+                { name: "Home", url: "/" },
+                { name: "Corporate Cards ", url: "/corporate-cards" },
+                {
+                  name: "Diy Card",
+                  url: "/corporate-cards/diy-card",
+                },
+              ]}
+            />
+          </div>
+          <div className={`${styles.title} col-12 `}>
+            <DynamicHeading
+              content={[
+                {
+                  title: "Diy Card",
+                  color: "color-equity-blue underline",
+                },
+              ]}
+              headingTag="p"
+              className="mb-2"
+            />
+
+            <div className="d-flex  flex-column text-center">
+              <DynamicHeading
+                content={[
+                  {
+                    title: "Take Control of Your Business Expenses with",
+                    color: "color-black f-3 italic d-block",
+                  },
+                  {
+                    title: " DIY Corporate Card Module",
+                    color: "color-black ",
+                  },
+                ]}
+                headingTag="h1"
+                className="f-7 mb-2"
+              />
+            </div>
+
+            <div className="d-inline text-center">
+              <DynamicHeading
+                content={[
+                  {
+                    title:
+                      "Design cards for specific needs, enforce spending policies, and oversee all transactions with ease.",
+                    color: "color-black subHeading",
+                  },
+                ]}
+                headingTag="p"
+                className=""
+              />
+            </div>
+            <div className={styles.button_wrapper}>
+              <RectangleButton title="Get Started" theme="blue" url="/sales" />
+            </div>
+          </div>
+        </div>
+        <div className=" col-12 pt-5 ">
+          <div className={styles.lottie_container}>
+            {" "}
+            <Image src={heroCardImg} alt="card background" className=" " />
+          </div>
+          <div className={styles.lottie_container_bottom}>
+            {" "}
+            <Image
+              src={podiumImage}
+              alt="card background"
+              className="position-absolute "
+            />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <LogoSlider />
+      </div>
+
+      <div className={`${styles.action_row} bg-white `}>
+        <div className="max-w-auto">
+          <div className={`${styles.title} text-center pb-md-5 pb-5`}>
+            <div>
+              <DynamicHeading
+                content={[
+                  {
+                    title: "How to Use the DIY Card Module",
+                    color: "color-black",
+                  },
+                ]}
+                headingTag="h2"
+                className="f-6"
+              />
+            </div>
+          </div>
+          <div className={`row bg-white align-items-center ${styles.section}`}>
+            <div className="col-md-6 col-12 px-md-5">
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "12px",
+                }}
+              >
+                {intantActionData.map(({ icon, title, description }, i) => (
+                  <div key={i} style={{ direction: "ltr" }}>
+                    <StepCard
+                      icon={icon}
+                      title={title}
+                      description={description}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className={`${styles.list_button}`}>
+                <RectangleButton
+                  title="Get started "
+                  theme="border-gray"
+                  actionImage={blueArrow}
+                  hoverImage={whiteArrow}
+                  url="/sales/?source=expense_management"
+                  className="d-flex justify-content-between align-items-center"
+                />
+              </div>
+            </div>
+            <div className={`col-md-6 col-12 `}>
+              <div>
+                <Image
+                  src={mealCardImage}
+                  alt="card background"
+                  className="w-100 mh-550 object-fit-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.card_stacking_row}>
+        <div className={` max-w-auto  ${styles.section}`}>
+          <>
+            <div className={`${styles.title} text-center `}>
+              <DynamicHeading
+                content={[
+                  {
+                    title: "The Smarter Approach to",
+                    color: "color-black",
+                  },
+                  {
+                    title: "Corporate Card Management ",
+                    color: "color-equity-blue",
+                  },
+                ]}
+                headingTag="h2"
+                className="f-6"
+              />
+            </div>
+
+            <CardStacking cards={cards} />
+          </>
+        </div>
+      </div>
+
+      <div className={`${styles.third_row} `}>
+        <div className="max-w-auto">
+          <div className={`${styles.title} text-center pb-2 pb-md-5`}>
+            <div
+              className={` flex-column justify-content-center align-items-center pb-3 `}
+            >
+              <DynamicHeading
+                content={[
+                  {
+                    title: "The EnKash Advantage - Benefits of Using EnKash’s ",
+                    color: "color-black",
+                  },
+                  {
+                    title: "DIY Card Module",
+                    color: "color-equity-blue",
+                  },
+                ]}
+                headingTag="h2"
+                className="f-6"
+              />
+            </div>
+          </div>
+          <div className={`row align-items-center ${styles.section}`}>
+            <div className="col-md-6 col-12  pe-md-5">
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "12px",
+                }}
+              >
+                {spendAnalyticsData.map(({ icon, title, description }, i) => (
+                  <div key={i} style={{ direction: "ltr" }}>
+                    <StepCard
+                      icon={icon}
+                      title={title}
+                      description={description}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className={`${styles.list_button}`}>
+                <RectangleButton
+                  title="Get started "
+                  theme="border-gray"
+                  actionImage={blueArrow}
+                  hoverImage={whiteArrow}
+                  url="/sales"
+                  className="d-flex justify-content-between align-items-center"
+                />
+              </div>
+            </div>
+            <div className={`col-md-6 col-12 d-flex `}>
+              <div>
+                <Image
+                  src={instantActionImg}
+                  alt="card background"
+                  className="w-100 mh-550 object-fit-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${styles.sixth_row} `}>
+        <div className="d-flex justify-content-center  flex-column gap-32   align-items-center max-w-auto">
+          <div className="d-flex justify-content-center   flex-column gap-4 align-items-center text-center">
+            <DynamicHeading
+              content={[
+                {
+                  title: "Take Charge of Your Expenses Today!",
+                  color: "color-white",
+                },
+              ]}
+              headingTag="h3"
+              className=""
+            />
+          </div>
+
+          <div className={`${styles.get_started_button} `}>
+            <RectangleButton
+              title="Get Started Today"
+              theme="outline-blue"
+              actionImage={blueArrow}
+              hoverImage={whiteArrow}
+              url="/sales/?source=expense_management"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={`${styles.faq_new_row}  relative`}>
+        <div className={`${styles.faqSection} text-start max-w-auto `}>
+          <div className={`${styles.title} text-start  pb-md-5 pb-3`}>
+            <DynamicHeading
+              content={[
+                {
+                  title: "Frequently Asked Questions (",
+                  color: "color-black",
+                },
+                {
+                  title: "FAQs",
+                  color: "color-equity-blue",
+                },
+                {
+                  title: ")",
+                  color: "color-black",
+                },
+              ]}
+              headingTag="h2"
+              className="f-6"
+            />
+          </div>
+          <div className="d-flex flex-column flex-md-row justify-content-between">
+            <div>
+              <div>
+                <DynamicHeading
+                  content={[
+                    {
+                      title: "Have more questions?",
+                      color: "color-dark-grey subHeading",
+                    },
+                  ]}
+                  headingTag="p"
+                  className="mb-0"
+                />
+              </div>
+              <div className="mt-3 d-none d-md-block">
+                <RectangleButton
+                  title="Get started today"
+                  theme="border-gray"
+                  actionImage={blueArrow}
+                  hoverImage={whiteArrow}
+                  iconSize={15}
+                  url="/sales/?source=receivables"
+                />
+              </div>
+            </div>
+            <div className={`${styles.faqData}`}>
+              <FAQHtml faqData={faqData} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.other_products}>
+        <div className="max-w-auto">
+          <div className={`${styles.title} text-center pb-5`}>
+            <DynamicHeading
+              content={[
+                {
+                  title: "Your Business, ",
+                  color: "color-black",
+                },
+                {
+                  title: "Your Cards – ",
+                  color: "color-equity-blue",
+                },
+                {
+                  title: "Tailored by EnKash",
+                  color: "color-black",
+                },
+              ]}
+              headingTag="h2"
+              className=""
+            />
+          </div>
+          <div className="row g-3 pb-4">
+            {cardType.map(({ titleHtml, description, cardImage }, index) => (
+              <div key={index} className="col-12 col-md-4">
+                <CardProduct
+                  titleHtml={titleHtml}
+                  description={description}
+                  cardImage={cardImage}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default DiyCards
