5769fef2080b561fe265b367d0f6d703474b2876
surajprasad-enkash <suraj.prasad@enkash.com>
2025-08-11 11:17:41 +0530

    changes in loder in expense page

--- components/contentShowcase/contentShowcase.tsx
+++ components/contentShowcase/contentShowcase.tsx
@@ -3,7 +3,7 @@ import DynamicHeading from "@/components/dynamicHeading/dynamic-heading"
 import RectangleButton from "@/components/buttons/rectangle-button/rectangle-button"
 import PolicyCard from "@/components/policyCard/policyCard"
 import styles from "./contentShowcase.module.scss"
-import { JSX, memo, Suspense } from "react"
+import { JSX, memo } from "react"
 
 interface ContentShowcaseProps {
   mainHeading?: { title: string; color: string }[]
@@ -35,73 +35,71 @@ function ContentShowcase({
   buttonUrl,
 }: ContentShowcaseProps): JSX.Element {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className={`${bgColor} ${styles.ContentShowcase} `}>
-        <div className="max-w-auto">
-          {mainHeading && (
-            <div className={`text-center  ${styles.MaineHeading} `}>
-              <DynamicHeading
-                content={mainHeading}
-                headingTag="h2"
-                className="f-6"
-              />
-            </div>
-          )}
-          <div className={`${styles.ContentShowcaseContent} `}>
-            <div className={`row  ${reverse ? "flex-row-reverse" : ""}`}>
-              <div
-                className={`col-md-6 col-12 ${
-                  reverse ? "order-2 order-md-1" : "order-1 order-md-1"
-                }`}
-              >
-                <div>
-                  <Image
-                    src={imageSrc}
-                    alt={imageAlt}
-                    className="position-relative w-100 mh-550 object-fit-contain"
-                  />
-                </div>
+    <div className={`${bgColor} ${styles.ContentShowcase} `}>
+      <div className="max-w-auto">
+        {mainHeading && (
+          <div className={`text-center  ${styles.MaineHeading} `}>
+            <DynamicHeading
+              content={mainHeading}
+              headingTag="h2"
+              className="f-6"
+            />
+          </div>
+        )}
+        <div className={`${styles.ContentShowcaseContent} `}>
+          <div className={`row  ${reverse ? "flex-row-reverse" : ""}`}>
+            <div
+              className={`col-md-6 col-12 ${
+                reverse ? "order-2 order-md-1" : "order-1 order-md-1"
+              }`}
+            >
+              <div>
+                <Image
+                  src={imageSrc}
+                  alt={imageAlt}
+                  className="position-relative w-100 mh-550 object-fit-contain"
+                />
               </div>
-              <div
-                className={`col-md-6 col-12 ${
-                  reverse ? "order-1 order-md-2" : "order-2 order-md-2"
-                }`}
-              >
-                <div className={`d-flex flex-column ${styles.subtitle}`}>
-                  <DynamicHeading
-                    content={[{ title: heading, color: "color-black" }]}
-                    headingTag="h4"
-                    className="f-5"
+            </div>
+            <div
+              className={`col-md-6 col-12 ${
+                reverse ? "order-1 order-md-2" : "order-2 order-md-2"
+              }`}
+            >
+              <div className={`d-flex flex-column ${styles.subtitle}`}>
+                <DynamicHeading
+                  content={[{ title: heading, color: "color-black" }]}
+                  headingTag="h4"
+                  className="f-5"
+                />
+                <DynamicHeading
+                  content={[{ title: subheading, color: "color-main-grey" }]}
+                  headingTag="p"
+                  className="f-4 mt-2 mb-0"
+                />
+              </div>
+              <div className="d-flex flex-column gap-32 mt-4">
+                {data.map(({ icon, title, description }, i) => (
+                  <PolicyCard
+                    key={i}
+                    icon={icon}
+                    title={title}
+                    description={description}
                   />
-                  <DynamicHeading
-                    content={[{ title: subheading, color: "color-main-grey" }]}
-                    headingTag="p"
-                    className="f-4 mt-2 mb-0"
-                  />
-                </div>
-                <div className="d-flex flex-column gap-32 mt-4">
-                  {data.map(({ icon, title, description }, i) => (
-                    <PolicyCard
-                      key={i}
-                      icon={icon}
-                      title={title}
-                      description={description}
-                    />
-                  ))}
-                </div>
-                <div className={styles.list_button}>
-                  <RectangleButton
-                    title={buttonTitle || "Get Started"}
-                    theme="outline-blue"
-                    url={buttonUrl || "/sales"}
-                  />
-                </div>
+                ))}
+              </div>
+              <div className={styles.list_button}>
+                <RectangleButton
+                  title={buttonTitle || "Get Started"}
+                  theme="outline-blue"
+                  url={buttonUrl || "/sales"}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-    </Suspense>
+    </div>
   )
 }
 
--- components/index.ts
+++ components/index.ts
@@ -1,29 +1,29 @@
-import React from "react"
-const Header = React.lazy(() => import("./header/header"))
-const Footer = React.lazy(() => import("./footer/footer"))
-const Heading = React.lazy(() => import("./heading/heading"))
-const FAQHtml = React.lazy(() => import("./faq-new/faqHtml"))
-const DynamicHeading = React.lazy(() => import("./dynamicHeading/dynamic-heading"))
-const LottieDynamicLoadComponent = React.lazy(() => import("./lottie-client/lottie-dynamic-load-client"))
-const CustomBreadcrumb = React.lazy(() => import("./breadcrumb/breadbrumb"))
-const TalkToSales = React.lazy(() => import("./mobile-talks-to-sales/mobile-talk-to-sales"))
-const AllProducts = React.lazy(() => import("./all-products/all-products"))
-const CardProduct = React.lazy(() => import("./card-product/card-product"))
-const LogoSlider = React.lazy(() => import("./logo-slider/logo-slider"))
-const PolicyCard = React.lazy(() => import("./policyCard/policyCard"))
-const FeatureCard = React.lazy(() => import("./featureCard/feature-card"))
-const StepCard = React.lazy(() => import("./stepCard/stepCard"))
-const AllInOnePolicy = React.lazy(() => import("./all-in-one-policy/all-in-one-policy"))
-const RectangleButton = React.lazy(() => import("./buttons/rectangle-button/rectangle-button"))
-const CardStacking = React.lazy(() => import("@/components/cardStacking/cardStacking"))
-const ContentShowcase = React.lazy(() => import("@/components/contentShowcase/contentShowcase"))
-const EnkashWay = React.lazy(() => import("@/components/enkash-way/enkash-way"))
-const SecondFaqHtml = React.lazy(() => import("@/components/second-faq/secondFaqHtml"))
-const ManagementCard = React.lazy(() => import("@/components/management-card/management-card"))
-const SplineAnimation = React.lazy(() => import("@/components/splineAnimation/splineAnimation"))
-const FeatureSpotlight = React.lazy(() => import("@/components/featureSpotlight/featureSpotlight"))
-const SmartPolicySection = React.lazy(() => import("@/components/smartPolicySection/smartPolicySection"))
-const CTASection = React.lazy(() => import("@/components/CTASection/CTASection"))
+import Footer from "./footer/footer"
+import Header from "./header/header"
+import Heading from "./heading/heading"
+import { FAQHtml } from "./faq-new"
+import DynamicHeading from "./dynamicHeading/dynamic-heading"
+import LottieDynamicLoadComponent from "./lottie-client/lottie-dynamic-load-client"
+import CustomBreadcrumb from "./breadcrumb/breadbrumb"
+import TalkToSales from "./mobile-talks-to-sales/mobile-talk-to-sales"
+import AllProducts from "./all-products/all-products"
+import CardProduct from "./card-product/card-product"
+import LogoSlider from "./logo-slider/logo-slider"
+import PolicyCard from "./policyCard/policyCard"
+import FeatureCard from "./featureCard/feature-card"
+import StepCard from "./stepCard/stepCard"
+import AllInOnePolicy from "./all-in-one-policy/all-in-one-policy"
+import CardStacking from "./cardStacking/cardStacking"
+import RectangleButton from "./buttons/rectangle-button/rectangle-button"
+import CTASection from "./CTASection/CTASection"
+import SmartPolicySection from "./smartPolicySection/smartPolicySection"
+import FeatureSpotlight from "./featureSpotlight/featureSpotlight"
+import EnkashWay from "./enkash-way/enkash-way"
+import SplineAnimation from "./splineAnimation/splineAnimation"
+import ContentShowcase from "./contentShowcase/contentShowcase"
+import SecondFaqHtml from "./second-faq/secondFaqHtml"
+import ManagementCard from "./management-card/management-card"
+ 
 export {
   Header,
   Footer,
@@ -42,12 +42,13 @@ export {
   StepCard,
   CardStacking,
   AllInOnePolicy,
-  ContentShowcase,
-  EnkashWay,
-  SecondFaqHtml,
-  ManagementCard,
-  SplineAnimation,
-  FeatureSpotlight,
+  CTASection,
   SmartPolicySection,
-  CTASection
+  FeatureSpotlight,
+  SecondFaqHtml,
+  EnkashWay,
+  SplineAnimation,
+  ContentShowcase,
+  ManagementCard
 }
+ 
