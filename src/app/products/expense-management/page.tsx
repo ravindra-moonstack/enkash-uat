818e27c9c4fbff13a67afbc1cf7d5e1ab546cdfa
surajprasad-enkash <suraj.prasad@enkash.com>
2025-08-12 11:45:01 +0530

    changes in expense page

--- src/app/products/expense-management/page.tsx
+++ src/app/products/expense-management/page.tsx
@@ -10,7 +10,6 @@ import {
   FAQHtml,
   ContentShowcase,
   AllInOnePolicy,
-  SplineAnimation,
   DynamicHeading,
   EnkashWay,
   LottieDynamicLoadComponent,
@@ -77,9 +76,9 @@ const ExpenseManagement = (): React.JSX.Element => {
       <Header utmSource="expense_management" />
       <TalkToSales />
       <div className={`${styles.first_row}`}>
-        <div className={`${styles.spline_background}`}>
+        {/* <div className={`${styles.spline_background}`}>
           <SplineAnimation />
-        </div>
+        </div> */}
         <div className="max-w-auto  position-relative no-pointer">
           <div className="d-flex">
             <CustomBreadcrumb
