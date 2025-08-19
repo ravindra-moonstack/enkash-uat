cfc63c1252704400f5f1370e04aafb8661c88edd
surajprasad-enkash <suraj.prasad@enkash.com>
2025-08-19 18:13:06 +0530

    new logo added

--- components/header/index.ts
+++ components/header/index.ts
@@ -1,4 +1,4 @@
-export { default as enkashBlueLogo } from "./img/enkash-blue-logo.png"
+export { default as enkashBlueLogo } from "./img/enkash-blue-logo.svg"
 export { default as enkashWhiteLogo } from "./img/enkash-white-logo.png"
 export { default as arrowUpBlue } from "./img/arrow-up-blue.png"
 export { default as arrowDownWhite } from "./img/arrow-down-white.png"
