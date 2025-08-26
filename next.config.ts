023f02b86e38febb3c048845324f10406a89948d
dheerajgill25 <dheerajgill25@gmail.com>
2025-08-26 18:12:15 +0530

    fix: config file fixed

--- next.config.ts
+++ next.config.ts
@@ -8,50 +8,42 @@ const withBundleAnalyzerConfigured = withBundleAnalyzer({
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    optimizeCss: true,
+    // optimizeCss: true,
     optimizePackageImports: ["@gsap/react", "react-icons"],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ]
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // Apply these headers to all routes in your application.
-  //       source: "/(.*)",
-  //       headers: [
-  //         {
-  //           key: "Strict-Transport-Security",
-  //           value: "max-age=63072000; includeSubDomains; preload",
-  //         },
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value:
-  //             "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self';",
-  //         },
-  //         {
-  //           key: "X-Frame-Options",
-  //           value: "DENY",
-  //         },
-  //         {
-  //           key: "X-Content-Type-Options",
-  //           value: "nosniff",
-  //         },
-  //         {
-  //           key: "Referrer-Policy",
-  //           value: "strict-origin-when-cross-origin",
-  //         },
-  //         {
-  //           key: "Permissions-Policy",
-  //           value: "camera=(), microphone=(), geolocation=()",
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
 }
 
 module.exports = withBundleAnalyzerConfigured(nextConfig)
