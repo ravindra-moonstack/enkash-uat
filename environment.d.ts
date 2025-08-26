4b215ed1c3361f135a7f7069f03d58b750b4a5a1
dheerajgill25 <dheerajgill25@gmail.com>
2025-08-26 16:14:15 +0530

    feat: code review installation

---
+++ environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_ZOHO_SALES_URL: string
+    NEXT_PUBLIC_ZOHO_PARTNERSHIP_URL: string
+    NEXT_PUBLIC_ZOHO_SUPPORT_URL: string
+    NEXT_PUBLIC_ZOHO_CONTACT_URL: string
+    NEXT_PUBLIC_ZOHO_AFFILIATE_URL: string
+  }
+}
