c0539b5af2aa77e464a29c1424fd109461a1d853
surajprasad-enkash <suraj.prasad@enkash.com>
2025-08-28 15:28:17 +0530

    fix: partner form added

--- components/paymentGatewayForm/index.tsx
+++ components/paymentGatewayForm/index.tsx
@@ -8,7 +8,6 @@ import { useFormik } from "formik"
 import "../../styles/_forms.scss"
 
 // components
-import { DynamicHeading } from "@/components"
 import CategoryWithOther from "../categoryWithOther/categoryWithOther"
 import ErrorText from "../ErrorText"
 
@@ -72,14 +71,6 @@ const PaymentGatewayPartnershipForm: React.FC = () => {
   return (
     <div className={"contactFormWrapper"}>
       <form action="#" onSubmit={handleSubmit}>
-        <DynamicHeading
-          content={[
-            { title: "Payment Gateway ", color: "color-black " },
-            { title: "Partnerships", color: "color-dark-grey " },
-          ]}
-          headingTag="h2"
-          className="text-center "
-        />
         <p className={"subtitle"}>We just need a few quick details</p>
 
         <div className={"grid"}>
