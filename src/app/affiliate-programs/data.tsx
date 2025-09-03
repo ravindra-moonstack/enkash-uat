950e356dc5b13588e93544d4f825b5d38ad095b1
surajprasad-enkash <suraj.prasad@enkash.com>
2025-09-03 14:19:33 +0530

    fix: extra data remove

--- src/app/affiliate-programs/data.tsx
+++ src/app/affiliate-programs/data.tsx
@@ -1,6 +1,7 @@
 import imaCard from "./img/containerScreen.webp"
 import ItemIcon from "./img/itemIcon.svg"
-
+import { coBrandImage,  GalaxyTabImage } from "."
+import { blueArrow } from "../home"
 const dataSets = [
   {
     // iconSrc: iconOne,
@@ -105,7 +106,34 @@ const BankSolutionHeading = {
   headingTag: "h2",
   className: "f-6 mb-3 mb-md-3",
 }
-
+const productsData = [
+  {
+    id: 1,
+    title: "Co-branded Products",
+    description:
+      "EnKash Co-branded Cards and Virtual Wallets empower businesses with smart, scalable, and fully controlled spending solutions. Our cards offer real-time visibility, customizable limits, and seamless integration with your finance workflows. Simplify your financial operations and gain full control without compromising on flexibility or speed.",
+    image: coBrandImage.src,
+    alt: "Co-branded Products Illustration",
+    button: {
+      connectText: "Explore Corporate Cards",
+      connectUrl: "/products/corporate-cards",
+      blueArrow: blueArrow.src,
+    },
+  },
+  {
+    id: 2,
+    title: "Payment Gateway",
+    description:
+      "EnKash Payment Gateway is an advanced solution designed specifically for Indian SMBs and startups. It empowers businesses of all sizes to accept payments seamlessly, securely, and at scale, giving them a fair chance to compete in a rapidly growing digital economy.",
+    image: GalaxyTabImage.src,
+    alt: "Payment Gateway Dashboard",
+    button: {
+      connectUrl: "/payment-gateway",
+      connectText: "Explore Payment Gateway",
+      blueArrow: blueArrow.src,
+    },
+  },
+]
 export {
   dataSets,
   imaCard,
@@ -114,4 +142,5 @@ export {
   headingData,
   stepsData,
   BankSolutionHeading,
+  productsData,
 }
