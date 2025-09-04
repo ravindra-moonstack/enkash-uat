8b9754a2613b266d566d2862cc97021a9cd43e95
surajprasad-enkash <suraj.prasad@enkash.com>
2025-09-04 14:29:39 +0530

    fix: homepage meta data added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from "next"
+import generateMetaData from "../utils/metaData"
 import HomePage from "./home/page"
 
+export const metadata: Metadata = generateMetaData({
+  title: "The Best Payments and Spend Management Platform",
+  description:
+    "Unlock growth with the best payments and spend management platform with products across corporate cards, vouchers, loyalty and more",
+  alternates: {
+    canonical: `${process.env.URL}`,
+  },
+})
 const Home = (): React.JSX.Element => {
   return <HomePage />
 }
