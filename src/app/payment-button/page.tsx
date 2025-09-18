76358916d0030434d2043cf9940844b1e77469a6
surajprasad-enkash <suraj.prasad@enkash.com>
2025-09-18 15:50:36 +0530

    fix: remove unused images of payment-gateway

--- src/app/payment-button/page.tsx
+++ src/app/payment-button/page.tsx
@@ -349,6 +349,7 @@ const PaymentButton = (): React.JSX.Element => {
           </div>
         </div>
       </div>
+      
       <div className={`${styles.eigth_row} `}>
         <div className={`${styles.faqSection} text-start  max-w-auto`}>
           <div className={`${styles.title} text-start pb-md-3 pb-3 col-md-8`}>
