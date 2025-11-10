7ce496cf9c7a07670cc6971db39e9b2c868e3089
surajprasad-enkash <suraj.prasad@enkash.com>
2025-11-10 17:44:43 +0530

    fix: changes in gap

--- src/app/products/expense-management/page.tsx
+++ src/app/products/expense-management/page.tsx
@@ -103,7 +103,7 @@ const ExpenseManagement = (): React.JSX.Element => {
           </div>
 
           <div className="col-12">
-            <div className="text-center mt-4 mt-mb-5">
+            <div className="text-center mt-3 mt-mb-5">
               <DynamicHeading
                 content={[
                   {
