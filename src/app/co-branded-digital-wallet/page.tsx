0703826d92271a9cd3e59caf7721de5462944506
surajprasad-enkash <suraj.prasad@enkash.com>
2025-11-14 12:08:01 +0530

    fix: changes in info section

--- src/app/co-branded-digital-wallet/page.tsx
+++ src/app/co-branded-digital-wallet/page.tsx
@@ -112,8 +112,8 @@ const CoBrandedDigitalWallet = (): React.JSX.Element => {
                 <div className={`d-flex ${styles.outerCard}`}>
                   <DynamicHeading
                     content={[{ title: card.value, color: "color-white" }]}
-                    headingTag="h2"
-                    className="f-6"
+                    headingTag="p"
+                    className="f-6 title-adaptive "
                   />
                 </div>
                 <div className={`py-2  ${styles.innerCard}`}>
