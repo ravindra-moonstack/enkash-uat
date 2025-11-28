aa89d59500d5c5e2cf54a93ca35cebf89e7e76cb
surajprasad-enkash <suraj.prasad@enkash.com>
2025-11-28 16:12:03 +0530

    fix: add blur effect in tab section

--- src/components/sections/hero-section/index.tsx
+++ src/components/sections/hero-section/index.tsx
@@ -24,6 +24,7 @@ interface HeroSectionProps {
     title: string
     url: string
     apiUrl?: string
+    vedioLink?: string
     theme?: "blue" | "black" | "white"
   }
   rightImage: StaticImageData | string
@@ -128,6 +129,13 @@ const HeroSection: React.FC<HeroSectionProps> = ({
                         title="API Doc"
                         theme="outline-blue"
                         url={button.apiUrl}
+                      />
+                    )}
+                    {button.vedioLink && (
+                      <CommanButton
+                        title="Watch Vedio"
+                        theme="blue"
+                        url={button.vedioLink}
                       />
                     )}
                   </div>
