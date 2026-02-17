1d643b3bc1c699e760926acbd01dd833f171c41d
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-02-17 17:57:16 +0530

    fix: cwv of some pages

--- src/app/policies/platform/tnc/page.tsx
+++ src/app/policies/platform/tnc/page.tsx
@@ -5,8 +5,11 @@ import Link from "next/link"
 import styles from "./page.module.scss"
 import generateMetaData from "@/src/utils/metaData"
 
-import DynamicHeading from "@/src/components/dynamic-heading"
-import CustomBreadcrumb from "@/src/components/breadcrumb"
+import dynamic from "next/dynamic"
+
+// Dynamic imports
+const DynamicHeading = dynamic(() => import("@/src/components/dynamic-heading"))
+const CustomBreadcrumb = dynamic(() => import("@/src/components/breadcrumb"))
 
 export const metadata: Metadata = generateMetaData({
   title: "Terms and Conditions (Platform): EnKash",
