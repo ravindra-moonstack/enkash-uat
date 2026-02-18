b5ebf759b6623a8e96717bd3e135b79607b19f4e
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-02-18 13:09:56 +0530

    fix: some more pages cwv

--- src/app/products/collect-payments/img/index.tsx
+++ src/app/products/collect-payments/img/index.tsx
@@ -4,22 +4,22 @@ export { default as affordabilityIcon } from "./afordability-suits-icon.svg"
 export { default as affordabilityImg } from "./afordability-suits-img.webp"
 
 export { default as paymentButtonIcon } from "./payment-button-icon.svg"
-export { default as paymentButtonImg } from "./payment-button.png"
+export { default as paymentButtonImg } from "./payment-button.webp"
 
 export { default as paymentGatwayIcon } from "./payment-gateway-icon.svg"
-export { default as paymentGatwayImg } from "./payment-gateway.png"
+export { default as paymentGatwayImg } from "./payment-gateway.webp"
 
 export { default as paymentLinkIcon } from "./payment-links-icon.svg"
-export { default as paymentLinkImg } from "./payment-link.png"
+export { default as paymentLinkImg } from "./payment-link.webp"
 
 export { default as paymentPageIcon } from "./payment-page-icon.svg"
-export { default as paymentPageImg } from "./payment-page.png"
+export { default as paymentPageImg } from "./payment-page.webp"
 
 export { default as qrCodeIcon } from "./qr-code-icon.svg"
-export { default as qrCodeImg } from "./qr-code.png"
+export { default as qrCodeImg } from "./qr-code.webp"
 
 export { default as upiIcon } from "./upi-payment-icon.svg"
-export { default as upiImg } from "./upi.png"
+export { default as upiImg } from "./upi.webp"
 
 export { default as iconOne } from "./auto-collect.svg"
 export { default as iconTwo } from "./subsccribtion.svg"
