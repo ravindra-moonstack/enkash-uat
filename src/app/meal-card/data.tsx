749e753fb91207ef1dd625be173f71c1b7282698
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-03-09 13:15:31 +0530

    fix: petty cash changes

--- src/app/meal-card/data.tsx
+++ src/app/meal-card/data.tsx
@@ -87,7 +87,7 @@ const intantActionData = [
   {
     title: "Card Activation",
     description:
-      "Employees will be notified via SMS, WhatsApp, and e-mail about rewards and the redemption process.",
+      "Employees must complete KYC to activate their meal cards.",
     icon: fourthIcon,
   },
 ]
