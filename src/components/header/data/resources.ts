e36d3465d3e99384484276e0b165ebde9dcec627
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-03-09 18:00:34 +0530

    fix: revert changes

--- src/components/header/data/resources.ts
+++ src/components/header/data/resources.ts
@@ -21,7 +21,7 @@ const resourseProducts: TProductCategory[] = [
             name: "Blogs",
             description: "Trends, tips, and strategies",
             imageSrcHovered: blogsFilled,
-            link: `https://uat.blogs.enkash.com/`,
+            link: `${process.env.NEXT_PUBLIC_URL}/resources/blogs`,
           },
 
           {
