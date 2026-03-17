a6a17250184d2063b1fda816f839c38c4ab469ab
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-03-17 16:51:14 +0530

    feat: added dns

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,6 +1,7 @@
-import mysql from 'mysql2/promise';
+import { createPool } from "mysql2/promise"
+import { lookup } from "node:dns/promises"
 
-const pool = mysql.createPool({
+const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -8,6 +9,16 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
+  connectTimeout: 20000,
+  lookup: (
+    hostname: string,
+    opts: any,
+    cb: (err: Error | null, address?: string, family?: number) => void
+  ) => {
+    lookup(hostname, { family: 4 })
+      .then((res) => cb(null, res.address, res.family))
+      .catch((err) => cb(err))
+  },
+} as any)
 
-export default pool;
+export default pool
