72c3413fb6d48a3a1c4f58cb5a265d90eecc6d1f
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-06-08 12:05:26 +0530

    fix: campaign payment gateway bg image

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import next from "next"
 import path from "path"
 
 const port = parseInt(process.env.PORT || "3000", 10)
-const dev = process.env.NODE_ENV === "development"
+const dev = process.env.NODE_ENV === "development" || !process.env.NODE_ENV
 
 const app = next({ dev })
 const handle = app.getRequestHandler()
