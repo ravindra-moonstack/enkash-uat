996b119e53542744a79d36257da14ecaa5d1b215
ravindra-moonstack <ravindra@moonstack.co>
2026-06-30 11:04:57 +0530

    fix: added third party libraries npm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import { Inter } from "next/font/google"
-import Script from "next/script"
+import { GoogleTagManager } from "@next/third-parties/google"
 import LayoutClientWrapper from "@/src/components/layout-wrapper"
 import { WebVitals } from "@/src/components/web-vitals"
 import { ToastProvider } from "@/src/context/ToastContext"
@@ -53,26 +53,7 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.variable}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TR7NDT8C"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        <Script
-          id="gtm-script"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: /* HTML */ `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var
-            f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TR7NDT8C');`,
-          }}
-        />
+        <GoogleTagManager gtmId="GTM-TR7NDT8C" />
         <DeferredScripts />
         <WebVitals />
         <ToastProvider>
