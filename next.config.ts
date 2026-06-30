fb0d52b26ea356e4ffba2fc07a02bb9d6911577f
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-06-30 10:21:03 +0530

    fix: nginx changes

--- next.config.ts
+++ next.config.ts
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
   },
 
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     remotePatterns: [
       {
         protocol: "https",
