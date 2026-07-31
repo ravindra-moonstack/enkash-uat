4ebc99877089850b58ffc4354b7518476f77fa0a
Neeta-Agrawal <neeta.agrawal@enkash.com>
2026-07-31 10:49:44 +0530

    fix: caching issue

--- next.config.ts
+++ next.config.ts
@@ -111,7 +111,15 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
       {
         source: "/((?!_next/static|_next/image|.*\\..*).*)",
         headers: [
